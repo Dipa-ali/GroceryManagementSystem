625403c426f62c740e3ce549d1ef15b6f005f6f4
Dipa-ali <dipalipa1993@gmail.com>
2023-06-11 15:36:57 +0530

    initial import

---
+++ src/test/java/com/project/ApiProjecthibernet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjecthibernet1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
